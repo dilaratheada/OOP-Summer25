b26d9664d56050b8ad6d573229842a0d27e4254a
ada dilara ozgurel - 35465 <adaozgurel@gmail.com>
2025-03-11 10:17:47 +0000

    Add Person class

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-# change it to print your name
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-print("Hello, I am Piotr Brudny")
+    def intro(self):
+        return f"My name is {self.name} and i am {self.age} years old."
+
+me = Person("Ada Dilara", 19)
+print(me.intro())
