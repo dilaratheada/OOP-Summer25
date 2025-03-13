612788293f67c0bbfeecda70ff7190db8939ea23
ada dilara ozgurel - 35465 <adaozgurel@gmail.com>
2025-03-13 13:58:23 +0000

    Add new file

---
+++ student_details.py
@@ -0,0 +1,7 @@
+first_name = 'Ada'
+second_name = 'Dilara'
+last_name = 'Ozgurel'
+index_number = '35465'
+nationality = 'Turkish'
+starting_date = '13-03-2025'
+courses = ['Math2', 'OOP', 'Polish', 'Logistics']
---
+++ student_details_structure.py
@@ -0,0 +1,16 @@
+student1 = {
+  'first_name' : 'Ada',
+  'second_name' : 'Dilara',
+  'last_name' : 'Ozgurel',
+  'index_number' : '35465',
+  'nationality' : 'Turkish',
+  'starting_date' : '13-03-2025',
+  'courses' : ['Math2', 'OOP', 'Polish', 'Logistics']
+}
+
+print(student1['first_name'], student1['second_name'])
+print(student1['last_name'])
+print(student1['index_number'])
+print(student1['nationality'])
+print(student1['starting_date'])
+print(student1['courses'])
