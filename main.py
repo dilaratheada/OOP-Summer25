e8383be7ca88f812a7d80c8846dc7e3a2394b356
ada dilara ozgurel - 35465 <adaozgurel@gmail.com>
2025-03-13 14:07:42 +0000

    student_details

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 class Person:
-     def __init__(self, name, age):
-         self.name = name
-         self.age = age
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
  
-     def intro(self):
-         return f"My name is {self.name} and i am {self.age} years old."
+    def intro(self):
+        return f"My name is {self.name} and i am {self.age} years old."
  
- me = Person("Ada Dilara", 19)
- print(me.intro())
+me = Person("Ada Dilara", 19)
+print(me.intro())
