bd1bc1b07a9c480d5be6c12664f76d13f2c74ebf
ada dilara ozgurel - 35465 <adaozgurel@gmail.com>
2025-03-13 13:36:13 +0000

    Add new file

---
+++ data_types.py
@@ -0,0 +1,5 @@
+my_name = "Ada" #string
+may_age = "19" #int
+my_hight = "163.4" #float
+fav_colors = ["blue", "red", "yellow", "green"] #list
+student = {'name' : 'berk', 'gender' : 'male', 'age' : '18'} 
