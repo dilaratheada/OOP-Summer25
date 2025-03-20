20e2d8f41adfe5e6b8044a4fa51286ec3fac8e19
ada dilara ozgurel - 35465 <adaozgurel@gmail.com>
2025-03-20 13:18:16 +0000

    animals.py

---
+++ animals.py
@@ -0,0 +1,30 @@
+dog_dict = {
+    'name' : 'dog',
+    'group' : 'mammalia',
+    'number_of_legs' : 4,
+    'skills' : ['running', 'biting', 'swimming']
+}
+croc_dict = {
+    'name' : 'crocodile',
+    'group' : 'reptilia',
+    'number_of_legs' : 4,
+    'skills' : ['holding breath under water', 'strong jaw', 'predetor']
+}
+human_dict = {
+    'name' : 'human',
+    'group' : 'mammalia',
+    'number_of_legs' : 2,
+    'skills' : ['walking', 'talking','thinking']
+}
+chic_dict = {
+    'name' : 'chicken',
+    'group' : 'aves',
+    'number_of_legs' : 2,
+    'skills' : ['partially fly', 'clucking', 'eating']
+}
+fish_dict = {
+    'name' : 'fish',
+    'group' : 'vertebrates',
+    'number_of_legs' : 0,
+    'skills' : ['no need for oxygen','swimming', 'seeing 360']
+}
